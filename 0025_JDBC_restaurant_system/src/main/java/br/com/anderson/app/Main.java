bdb457056ee3cf48324eb4b597ac6fc5169afce3
anderson.paiva <anderson.paiva@quark.tec.br>
2025-03-16 21:28:31 -0300

    [0025] - finalizar exibicao de todos os menus inicias do sistema

--- 0025_JDBC_restaurant_system/src/main/java/br/com/anderson/app/Main.java
+++ 0025_JDBC_restaurant_system/src/main/java/br/com/anderson/app/Main.java
@@ -1,69 +1,123 @@
 package br.com.anderson.app;
 
+import br.com.anderson.utils.ScannerUtil;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void exibirMenu() {
         System.out.println("=====================================");
-        System.out.println("        MENU DE GERENCIAMENTO        ");
+        System.out.println("          Navegue pelo MENU          ");
         System.out.println("=====================================");
+        System.out.println("1 - Clientes");
+        System.out.println("2 - Funcionários");
+        System.out.println("3 - Produtos");
+        System.out.println("4 - Categorias de produtos");
+        System.out.println("5 - Pedidos");
+        System.out.println("6 - Relatórios de clientes");
+        System.out.println("7 - Relatórios de funcionários");
+        System.out.println("8 - Relatórios de vendas");
+        System.out.println("0 - Sair\n");
+    }
 
-        System.out.println("\n------ CLIENTES ------");
-        System.out.println("1  - Cadastrar cliente");
-        System.out.println("2  - Buscar cliente");
-        System.out.println("3  - Alterar dados do cliente");
-        System.out.println("4  - Remover cliente");
+    public static void menuCliente() {
+        System.out.println("1 - Cadastrar cliente");
+        System.out.println("2 - Buscar cliente");
+        System.out.println("3 - Alterar dados do cliente"); /* Escolher o dado */
+        System.out.println("4 - Remover cliente");
+        System.out.println("5 - Voltar");
+    }
 
-        System.out.println("\n------ FUNCIONÁRIOS ------");
-        System.out.println("5  - Cadastrar funcionário");
-        System.out.println("6  - Buscar funcionário");
-        System.out.println("7  - Alterar dados do funcionário");
-        System.out.println("8  - Remover funcionário");
+    public static void menuFuncionario() {
+        System.out.println("1 - Cadastrar funcionário");
+        System.out.println("2 - Buscar funcionário");
+        System.out.println("3 - Alterar dados do funcionário"); /* Escolher dado */
+        System.out.println("4 - Remover funcionário");
+        System.out.println("5 - Voltar");
+    }
 
-        System.out.println("\n------ PRODUTOS ------");
-        System.out.println("9  - Cadastrar produto");
-        System.out.println("10 - Buscar produto");
-        System.out.println("11 - Alterar dados do produto");
-        System.out.println("12 - Remover produto");
+    public static void menuProduto() {
+        System.out.println("1 - Cadastrar produto");
+        System.out.println("2 - Buscar produto");
+        System.out.println("3 - Alterar dados do produto"); /* Escolher dado */
+        System.out.println("4 - Remover produto");
+        System.out.println("5 - Voltar");
+    }
 
-        System.out.println("\n------ CATEGORIAS DE PRODUTOS ------");
-        System.out.println("13 - Cadastrar categoria de produto");
-        System.out.println("14 - Buscar categoria de produto");
-        System.out.println("15 - Alterar dados da categoria de produto");
-        System.out.println("16 - Remover categoria de produtos");
+    public static void menuCategoriaProduto() {
+        System.out.println("1 - Cadastrar categoria de produto");
+        System.out.println("2 - Buscar categoria de produto");
+        System.out.println("3 - Alterar dados da categoria de produto"); /* Escolher dado */
+        System.out.println("4 - Remover categoria de produto");
+        System.out.println("5 - Voltar");
+    }
 
-        System.out.println("\n------ PEDIDOS ------");
-        System.out.println("17 - Criar novo pedido");
-        System.out.println("18 - Buscar pedido por ID");
-        System.out.println("19 - Listar todos os pedidos");
-        System.out.println("20 - Alterar status do pedido");
-        System.out.println("21 - Remover pedido");
+    public static void menuPedido() {
+        System.out.println("1 - Criar novo pedido");
+        System.out.println("2 - Buscar pedido por ID");
+        System.out.println("3 - Listar todos os pedidos");
+        System.out.println("4 - Alterar status do pedido");
+        System.out.println("5 - Remover produto");
+        System.out.println("6 - Voltar");
+    }
 
-        System.out.println("\n------ RELATÓRIOS DE CLIENTES ------");
-        System.out.println("22 - Clientes com mais pedidos feitos");
-        System.out.println("23 - Listagem de clientes por ordem alfabética");
-        System.out.println("24 - Listagem de clientes por valor gasto");
+    public static void menuRelatorioClientes() {
+        System.out.println("1 - Clientes com mais pedidos feitos");
+        System.out.println("2 - Listagem de clientes por ordem alfabética");
+        System.out.println("3 - Listagem de clientes por valor gasto");
+        System.out.println("4 - Voltar");
+    }
 
-        System.out.println("\n------ RELATÓRIOS DE FUNCIONÁRIOS ------");
-        System.out.println("25 - Funcionário com mais pedidos processados");
-        System.out.println("26 - Listagem de funcionários por ganho");
-        System.out.println("27 - Funcionário do mês");
+    public static void menuRelatorioFuncionarios() {
+        System.out.println("1 - Funcionários com mais pedidos processados");
+        System.out.println("2 - Listagem de funcionários por ganho");
+        System.out.println("3 - Funcionário do mês");
+        System.out.println("4 - Voltar");
+    }
 
-        System.out.println("\n------ RELATÓRIOS DE VENDAS ------");
-        System.out.println("28 - Produto mais vendido");
-        System.out.println("29 - Listagem de produtos por número de vendas");
-        System.out.println("30 - Categoria de produtos mais vendida");
-        System.out.println("31 - Listagem de categorias de produtos por número de vendas");
-
-        System.out.println("\n0 - Sair");
-        System.out.println("=====================================");
-        System.out.print("Escolha uma opção: ");
+    public static void menuRelatorioVendas() {
+        System.out.println("1 - Produto mais vendido");
+        System.out.println("2 - Listagem de produtos por número de vendas");
+        System.out.println("3 - Categoria de produtos mais vendida");
+        System.out.println("4 - Listagem de categorias de produtos por número de vendas");
+        System.out.println("5 - Voltar");
     }
 
     public static void main(String[] args) {
-        System.out.println("Bem-vindo ao sistema de gerenciamento de pedidos do restaurante!");
+        Scanner sc = new Scanner(System.in);
+        ScannerUtil scannerUtil = new ScannerUtil(sc);
 
-        while(true){
+        exibirMenu();
+        int option = scannerUtil.requestInteger("Digite aqui");
 
+        switch(option) {
+            case 1:
+                menuCliente();
+                break;
+            case 2:
+                menuFuncionario();
+                break;
+            case 3:
+                menuProduto();
+                break;
+            case 4:
+                menuCategoriaProduto();
+                break;
+            case 5:
+                menuPedido();
+                break;
+            case 6:
+                menuRelatorioClientes();
+                break;
+            case 7:
+                menuRelatorioFuncionarios();
+                break;
+            case 8:
+                menuRelatorioVendas();
+                break;
+            default:
+                System.out.println("valor default");
         }
     }
 }
